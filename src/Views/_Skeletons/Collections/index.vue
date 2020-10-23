26017d4f1086e9474435d04002561c5ad5ac7c7f
André Carello <andrecarello@MacBook-Air-de-Andre.local>
2020-10-23 19:51:53 -0300

    [Oi Vantagens] Ajuste do skeleton

--- src/Views/_Skeletons/Collections/index.vue
+++ src/Views/_Skeletons/Collections/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div skeleton="collections">
-    <div class="title" />
-    <div class="items">
-      <div class="item" v-for="i in 5" :key="i">
+  <div class="skeleton">
+    <div class="skeleton:title" />
+    <div class="skeleton:items">
+      <div class="skeleton:item" v-for="i in 5" :key="i">
         <div v-for="i in 4" :key="i"></div>
       </div>
     </div>
